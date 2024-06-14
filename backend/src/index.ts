4a056f1a5ee700979a518038faafd6957a07d3cc
emiliano Gillini <emigillini@hotmail.com>
2024-06-14 13:50:28 -0300

    Added DataBase MongoDB + Connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,40 @@
-import  express  from "express";
+import express from "express";
+import mongoose from "mongoose";
 
-const app=express()
-app.use(express.json())
-const PORT = 3000
+const app = express();
+app.use(express.json());
+const PORT = 3000;
 
-app.get("/api",(req, res)=>{
-    const hiUser= "Hello New User"
-    res.send(hiUser)
-})
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`);
-})
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://admin:admin@cluster0.spvzvph.mongodb.net/"
+    );
+    console.log("Connection to the database successful");
+  } catch (error) {
+    console.error("Database connection error:", error);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
+app.get("/api", (req, res) => {
+  const hiUser = "Hello New User...";
+  res.send(hiUser);
+});
+app.listen(PORT, () => {
+  console.log(`server is running on port  ${PORT}`);
+});
+
+process.on("SIGINT", async () => {
+  console.log("SIGINT signal received: closing MongoDB connection");
+  try {
+    await mongoose.disconnect();
+    console.log("Disconnected from the database");
+  } catch (error) {
+    console.error("Error disconnecting from the database:", error);
+  } finally {
+    process.exit(0);
+  }
+});
