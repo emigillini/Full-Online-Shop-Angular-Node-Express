7aa5e4f0d0ec98d872e19b0f55143f49b34839b1
Emig <emigillini@hotmail.com>
2024-08-26 20:14:09 -0300

    Final Ajustments, preparing Deploy

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    ENDPOINT : 'http://localhost:3000/api/',
+    ENDPOINT : 'http://127.0.0.1:3000/api',
     COMPANYEMAIL : 'fullstorefullstore3@gmail.com',
     stripePublicKey :'pk_test_51PYFXKRpo1PRcWo6dNh516IaCUkX2vbYyXSfQjR2XMxQVWsTCNMFMV0SzBa6PQCA8BUuaVk1kz30jxXepbBx4Rd200s3NDluct',
 
